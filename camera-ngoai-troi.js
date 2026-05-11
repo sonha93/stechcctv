71479123efd0afc5aa2bc32de6ffc6fc6f3033fd
sonha93 <sonha93dh@gmail.com>
2026-05-12 00:02:52 +0700

    Update camera-ngoai-troi.js

--- camera-ngoai-troi.js
+++ camera-ngoai-troi.js
@@ -1,7 +1,7 @@
 
 /* =========================
    FIREBASE
-========================= */
+========================= */   
 
 import { initializeApp }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
