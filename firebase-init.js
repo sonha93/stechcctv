536b08f173cb846bd3927c7993356deec3c7752a
sonha93 <sonha93dh@gmail.com>
2026-05-13 15:05:58 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,34 +1,25 @@
-// ==========================
-// FIREBASE INIT
-// ==========================
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
 
+<script>
+  const firebaseConfig = {
+    apiKey:"AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
+    authDomain:"stech-73b89.firebaseapp.com",
+    databaseURL:"https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId:"stech-73b89",
+    storageBucket:"stech-73b89.appspot.com",
+    messagingSenderId:"873739162979",
+    appId:"1:873739162979:web:978f1a4043f025b1cdaf56",
+    measurementId:"G-98Q3927PHZ"
+  };
 
+  // Init Firebase
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
-  authDomain: "stech-73b89.firebaseapp.com",
-  databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "stech-73b89",
-  storageBucket: "stech-73b89.appspot.com",
-  messagingSenderId: "873739162979",
-  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
-};
-
-// Kiểm tra nếu Firebase chưa init thì init
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-// Lấy auth và database dùng chung
-window.auth = firebase.auth();
-window.db = firebase.database();
-window.storage = firebase.storage();
-
-// Biến trạng thái Firebase sẵn sàng
-let firebaseReady = false;
-
-// Đặt firebaseReady = true khi auth state check xong
-auth.onAuthStateChanged(user => {
-  firebaseReady = true;
-});
+  // Khai báo chung cho cart.js dùng
+  window.auth = firebase.auth();
+  window.db = firebase.database();
+</script>
