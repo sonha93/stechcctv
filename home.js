dc27d97a9b0684e2bc2434bc970e99d9600bac60
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-15 08:56:04 +0700

    Update home.js

--- home.js
+++ home.js
@@ -236,49 +236,29 @@ window.goDetail = function(id){
 ========================= */
 
 window.addToCart = function(id){
+  const auth = firebase.auth();
+  const user = auth.currentUser;
+  if(!user){
+    alert("Vui lòng đăng nhập!");
+    return;
+  }
 
-  const product =
-    allProducts.find(
-      p => String(p.id) === String(id)
-    );
+  const cartKey = "cart_" + user.uid;
 
+  const product = allProducts.find(p => String(p.id) === String(id));
   if(!product) return;
 
-  let cart =
-    JSON.parse(
-      localStorage.getItem("cart")
-    ) || [];
+  let cart = JSON.parse(localStorage.getItem(cartKey)) || [];
 
-  const exist =
-    cart.find(
-      i => String(i.id) === String(id)
-    );
-
+  const exist = cart.find(i => String(i.id) === String(id));
   if(exist){
-
     exist.qty += 1;
-
+  } else {
+    cart.push({...product, qty:1});
   }
 
-  else{
-
-    cart.push({
-
-      ...product,
-
-      qty:1
-
-    });
-
-  }
-
-  localStorage.setItem(
-    "cart",
-    JSON.stringify(cart)
-  );
-
+  localStorage.setItem(cartKey, JSON.stringify(cart));
   alert("Đã thêm vào giỏ 🛒");
-
 };
 
 /* =========================
