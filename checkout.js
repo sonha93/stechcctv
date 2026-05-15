b5af69cb27c985955ced015ddc7ca4521abce545
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-15 17:09:46 +0700

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -16,24 +16,24 @@ function formatPrice(n) {
    🖥 RENDER CHECKOUT
 ========================= */
 function renderCheckout() {
-  const box = document.getElementById("cart");
-  const totalBox = document.getElementById("total");
 
-  if (!box || !totalBox) return;
+  const box = document.getElementById("cartBox");
+
+  if (!box) return;
 
   const cart = getCart();
 
   box.innerHTML = "";
 
   if (cart.length === 0) {
-    box.innerHTML = "<p>Giỏ hàng trống</p>";
-    totalBox.innerText = "0";
+    box.innerHTML = "<p>🛒 Giỏ hàng trống</p>";
     return;
   }
 
   let total = 0;
 
   cart.forEach(item => {
+
     const price = Number(item.price) || 0;
     const oldPrice = Number(item.oldPrice) || 0;
     const qty = Number(item.qty) || 1;
@@ -43,35 +43,49 @@ function renderCheckout() {
     total += price * qty;
 
     box.innerHTML += `
-      <div class="cart-item">
+      <div class="item">
 
-        <img src="${item.img}" style="width:60px">
+        <img src="${item.img}" style="width:70px;height:70px;object-fit:cover">
 
         <div>
           <h4>${item.name}</h4>
 
           <div class="price-box">
-            <span class="price">${formatPrice(price)}</span>
+
+            <span class="sale-price">
+              ${formatPrice(price)}
+            </span>
 
             ${
               hasDiscount
                 ? `<span class="old-price">${formatPrice(oldPrice)}</span>`
                 : ""
             }
+
           </div>
 
           <p>Số lượng: ${qty}</p>
-          <p>Thành tiền: ${formatPrice(price * qty)}</p>
+
+          <p>
+            Thành tiền:
+            ${formatPrice(price * qty)}
+          </p>
+
         </div>
 
       </div>
-      <hr>
     `;
   });
 
-  totalBox.innerText = formatPrice(total);
+  box.innerHTML += `
+    <div class="total-box">
+      <div class="row final">
+        <span>Tổng thanh toán</span>
+        <b>${formatPrice(total)}</b>
+      </div>
+    </div>
+  `;
 }
-
 /* =========================
    🧹 XOÁ GIỎ HÀNG
 ========================= */
