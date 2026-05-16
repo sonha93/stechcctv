72878ee4b9642d8d863c8222f4f26eb1369a81a2
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-16 12:57:50 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -4,28 +4,29 @@
 
 // Cấu hình Firebase của bạn
 const firebaseConfig = {
-    apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
-    authDomain: "stech-73b89.firebaseapp.com",
-    databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "stech-73b89",
-    storageBucket: "stech-73b89.appspot.com",
-    messagingSenderId: "873739162979",
-    appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
+  apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
+  authDomain: "stech-73b89.firebaseapp.com",
+  databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "stech-73b89",
+  storageBucket: "stech-73b89.appspot.com",
+  messagingSenderId: "873739162979",
+  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
 };
 
 // Kiểm tra nếu Firebase chưa init thì init
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 // Lấy auth và database dùng chung
 const auth = firebase.auth();
 const db = firebase.database();
+const storage = firebase.storage(); // nếu cần upload hình ảnh
 
 // Biến trạng thái Firebase sẵn sàng
 let firebaseReady = false;
 
 // Đặt firebaseReady = true khi auth state check xong
 auth.onAuthStateChanged(user => {
-    firebaseReady = true;
+  firebaseReady = true;
 });
