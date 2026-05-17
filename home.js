e6c52258ac4d2e1e799d21a9f126f118cc2f1be3
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-17 09:31:57 +0700

    Update home.js

--- home.js
+++ home.js
@@ -6,24 +6,17 @@
    FIRESTORE INIT
 ========================= */
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { app } from "./auth.js";
+
 import {
   getFirestore,
   collection,
   getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
-  authDomain: "stech-73b89.firebaseapp.com",
-  projectId: "stech-73b89",
-  storageBucket: "stech-73b89.appspot.com",
-  messagingSenderId: "873739162979",
-  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
-};
-
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+
 
 /* =========================
    GET PRODUCTS FROM FIRESTORE
