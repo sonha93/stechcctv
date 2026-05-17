994bf297d086cb962b2a8ef5a2c337520a5a6502
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-17 09:04:15 +0700

    Update home.js

--- home.js
+++ home.js
@@ -2,9 +2,6 @@
 // IMPORT
 // ==========================
 
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
   getFirestore,
   collection,
@@ -17,33 +14,12 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import { app } from "./auth.js";
+
 import {
   addToCart
 }
 from "./cart.js";
-
-
-// ==========================
-// FIREBASE
-// ==========================
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
-
-  authDomain: "stech-73b89.firebaseapp.com",
-
-  projectId: "stech-73b89",
-
-  storageBucket: "stech-73b89.appspot.com",
-
-  messagingSenderId: "873739162979",
-
-  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
-
-};
-
-const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
