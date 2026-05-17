e7b74497f89874649ffaaecf08a7e49bc06012a6
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-17 22:44:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -157,16 +157,15 @@ window.addToCartById = function(id) {
     p => String(p.id) === String(id)
   );
 
+  console.log("FOUND PRODUCT:", product);
+
   if (!product) {
-
     console.error("Không tìm thấy sản phẩm");
-
     return;
   }
 
   addToCart(product);
 };
-
 window.addEventListener(
   "DOMContentLoaded",
   () => {
