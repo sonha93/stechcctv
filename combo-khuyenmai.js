ad04818f54e6b6d0a0917c81e7bb9d4e69026726
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-18 17:02:29 +0700

    Update combo-khuyenmai.js

--- combo-khuyenmai.js
+++ combo-khuyenmai.js
@@ -106,7 +106,7 @@ const featured = allProducts.filter(
         </div>
        
 </button>
-        <button class="cart-btn" onclick="addToCart('${id}')">🛒 Mua ngay</button>
+        <button class="cart-btn" onclick="addToCart('${id}')">🛒 Thêm vào giỏ</button>
        </div>
     `;
   });
