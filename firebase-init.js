646b3dbeac886c8020c072e901c2c60064e337cb
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-28 15:07:48 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -2,49 +2,33 @@
 // FIREBASE INIT
 // ==========================
 
+// Cấu hình Firebase của bạn
 const firebaseConfig = {
-
   apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
-
   authDomain: "stech-73b89.firebaseapp.com",
-
-  databaseURL:
-    "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
-
+  databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "stech-73b89",
-
   storageBucket: "stech-73b89.appspot.com",
-
   messagingSenderId: "873739162979",
-
-  appId:
-    "1:873739162979:web:978f1a4043f025b1cdaf56"
+  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
 };
 
-if (!firebase.apps.length) {
 
+// Kiểm tra nếu Firebase chưa init thì init
+if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+// Lấy auth và database dùng chung
 const auth = firebase.auth();
-
 const db = firebase.firestore();
+const storage = firebase.storage(); // nếu cần upload hình ảnh
 
-const rtdb = firebase.database();
-
-const storage = firebase.storage();
-
+// Biến trạng thái Firebase sẵn sàng
 let firebaseReady = false;
 
+// Đặt firebaseReady = true khi auth state check xong
 auth.onAuthStateChanged(user => {
-
   firebaseReady = true;
 });
-
-export {
-
-  auth,
-  db,
-  rtdb,
-  storage
-};
+export { auth, db, storage };
