c3e078861acbec446d1293feac0667cd6cb8bb02
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-05-31 14:20:13 +0700

    Update admin-orders.js

--- admin-orders.js
+++ admin-orders.js
@@ -124,19 +124,20 @@ const filterDate =
 const clearDate =
   document.getElementById("clearDate");
 
-const todayLabel =
-  document.getElementById("todayLabel");
 
-if(todayLabel){
-
-  todayLabel.textContent =
-    "Hôm nay: " +
-    new Date().toLocaleDateString("vi-VN");
-
-}
 // mặc định ngày hôm nay
-if(filterDate){
+if (filterDate) {
+
+  const today = new Date();
+
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const dd = String(today.getDate()).padStart(2, "0");
+
+  filterDate.value = `${yyyy}-${mm}-${dd}`;
+
 }
+
 // ============================
 // FILTER REVENUE RANGE
 // ============================
