a07fbb706c4f6d72c3e47997022a9b5e4ac3be1e
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-07 14:01:36 +0700

    Update inventory_audit.js

--- inventory_audit.js
+++ inventory_audit.js
@@ -1,3 +1,10 @@
+import { db } from "./firebase-init.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 let auditOpen = false;
 
 const auditStatus =
@@ -24,3 +31,45 @@ document
     "🔴 ĐANG ĐÓNG";
 
 });
+async function loadProducts(){
+
+    const auditProducts =
+    document.getElementById("auditProducts");
+
+    auditProducts.innerHTML = "";
+
+    const snap =
+    await getDocs(
+        collection(db,"products")
+    );
+
+    snap.forEach(docSnap => {
+
+        const p = docSnap.data();
+
+        auditProducts.innerHTML += `
+
+        <div class="audit-item">
+
+            <h3>${p.name}</h3>
+
+            <div>
+                📦 Tồn hệ thống:
+                <b>${p.stock || 0}</b>
+            </div>
+
+            <input
+                type="number"
+                placeholder="Nhập tồn thực tế"
+                data-id="${docSnap.id}"
+                class="actualStock"
+            >
+
+        </div>
+
+        `;
+
+    });
+
+}
+loadProducts();
