8df4916e8c71eae2ba8fcebdba001dc0e9dc4005
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-09 13:41:21 +0700

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -1,32 +1,43 @@
-import { auth } from "./firebase-config.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// admin-login.js
 
-const loginBtn = document.getElementById("loginBtn");
-const msg = document.getElementById("msg");
+const firebaseConfig = {
+  apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
+  authDomain: "stech-73b89.firebaseapp.com",
+  databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "stech-73b89",
+  storageBucket: "stech-73b89.appspot.com",
+  messagingSenderId: "873739162979",
+  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
+};
 
-loginBtn.addEventListener("click", loginAdmin);
-document.addEventListener("keydown", e => { if(e.key === "Enter") loginAdmin(); });
+firebase.initializeApp(firebaseConfig);
 
-async function loginAdmin() {
+function loginAdmin() {
+
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
+    const msg = document.getElementById("msg");
 
     if (!username || !password) {
         msg.innerText = "Nhập tài khoản và mật khẩu";
         return;
     }
 
-    try {
-        // admin email mặc định dạng: user + @admin.com
-        const email = username + "@admin.com";
+    const email = username + "@admin.com";
 
-        await signInWithEmailAndPassword(auth, email, password);
-
-        // login thành công
-        window.location.href = "admin-orders.html";
-
-    } catch(err) {
-        console.error(err);
-        msg.innerText = "Sai tài khoản hoặc mật khẩu";
-    }
+    firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => {
+            window.location.href = "admin-orders.html";
+        })
+        .catch((error) => {
+            console.error(error);
+            msg.innerText = "Sai tài khoản hoặc mật khẩu";
+        });
 }
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "Enter"){
+        loginAdmin();
+    }
+});
