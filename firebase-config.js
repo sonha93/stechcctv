1ce183e1305762d4327f54e7615f1d0d0a7b4e84
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-09 13:32:56 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,20 @@
 // firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
   authDomain: "stech-73b89.firebaseapp.com",
-   databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "stech-73b89",
-  storageBucket: "stech-73b89.appspot.com", // sửa lại chỗ này
+  storageBucket: "stech-73b89.appspot.com",
   messagingSenderId: "873739162979",
-  appId: "1:873739162979:web:978f1a4043f025b1cdaf56",
-  measurementId: "G-98Q3927PHZ"
+  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
 };
 
-// Khởi tạo Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
