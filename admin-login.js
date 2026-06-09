d2138cd318108d784acfcebf4ad91f960339f1b0
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-09 18:05:55 +0700

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -1,4 +1,16 @@
+// admin-login.js
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
+  authDomain: "stech-73b89.firebaseapp.com",
+  databaseURL: "https://stech-73b89-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "stech-73b89",
+  storageBucket: "stech-73b89.appspot.com",
+  messagingSenderId: "873739162979",
+  appId: "1:873739162979:web:978f1a4043f025b1cdaf56"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function loginAdmin() {
 
@@ -11,16 +23,16 @@ function loginAdmin() {
         return;
     }
 
-    const email = username + "@stech.com";
+    const email = username + "@admin.com";
 
-    adminAuth
+    firebase.auth()
         .signInWithEmailAndPassword(email, password)
         .then(() => {
             window.location.href = "admin-orders.html";
         })
         .catch((error) => {
-            msg.innerText = error.code;
-            console.log(error);
+            console.error(error);
+            msg.innerText = "Sai tài khoản hoặc mật khẩu";
         });
 }
 
