1489f4dfdaedd0529af035d1721661d7f680b5bf
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-18 11:02:31 +0700

    Update verified-users.js

--- verified-users.js
+++ verified-users.js
@@ -36,19 +36,6 @@ export function getVerifiedBadge(uid){
       d="M10.2 15.8l-3-3 1.4-1.4 1.6 1.6 5-5 1.4 1.4z"/>
     </svg>
 
-  <div class="verified-popup-title">
-
-  <svg class="verified-popup-icon" viewBox="0 0 24 24">
-    <path fill="#0866FF"
-      d="M12 0l2.6 2.1 3.3-.6 1.6 3 3.3.7-.7 3.3 2.1 2.5-2.1 2.5.7 3.3-3.3.7-1.6 3-3.3-.6L12 24l-2.6-2.1-3.3.6-1.6-3-3.3-.7.7-3.3L0 12l2.1-2.5-.7-3.3 3.3-.7 1.6-3 3.3.6z"/>
-    <path fill="#fff"
-      d="M10.2 15.8l-3-3 1.4-1.4 1.6 1.6 5-5 1.4 1.4z"/>
-  </svg>
-
-  <span>Tài khoản đã xác minh</span>
-
-</div>
-
   </span>
   `;
 }
