711ea5b7f19f8fcb2be88a750694910803284f4c
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-21 14:37:43 +0700

    Update admin-orders.js

--- admin-orders.js
+++ admin-orders.js
@@ -669,7 +669,7 @@ ${
 
           <option value="cancelled"
             ${order.status === "cancelled" ? "selected" : ""}>
-            Đã hủy
+            Hủy đơn
           </option>
 
         </select>
