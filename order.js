d75cc8a9b1588830a05a873feffe6dc4b955fb82
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-22 10:53:12 +0700

    Update order.js

--- order.js
+++ order.js
@@ -310,10 +310,10 @@ const finalTotal =
 
         <div class="item">
 
-<a href="logo.html?id=${item.productId || item.id}" style="display:inline-block;">
+<a href="logo.html?id=${item.productId || item.id}">
   <img 
     src="${item.img || ''}" 
-    style="cursor:pointer; display:block;"
+    style="cursor:pointer"
   >
 </a>
           <div style="flex:1;">
