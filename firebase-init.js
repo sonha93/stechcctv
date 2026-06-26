272a4d1cfbe36ad6d8783c4ebc5d3c6e10788841
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-26 15:34:03 +0700

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,7 @@
 // ==========================
 // FIREBASE INIT
 // ==========================
-import { ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 // Cấu hình Firebase của bạn
 const firebaseConfig = {
   apiKey: "AIzaSyDYVcBEYJN1HUCta3XdJAUBe4TGLnmy7y4",
@@ -22,7 +22,6 @@ if (!firebase.apps.length) {
 // Lấy auth và database dùng chung
 const auth = firebase.auth();
 const db = firebase.firestore();
-const rtdb = firebase.database();
 const storage =
 firebase.storage
 ? firebase.storage()
@@ -36,22 +35,3 @@ auth.onAuthStateChanged(user => {
   firebaseReady = true;
 });
 export { auth, db, storage };
-export function listenUnreadMessages(userId, callback) {
-  if (!userId) return;
-
-  const msgRef = ref(rtdb, "messages");
-
-  onValue(msgRef, (snapshot) => {
-    let count = 0;
-
-    snapshot.forEach(child => {
-      const data = child.val();
-
-      if (data.to === userId && data.read === false) {
-        count++;
-      }
-    });
-
-    callback(count);
-  });
-}
