2b58d22531d9dcdbfaf344bf6f077188161c5f32
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-06-26 21:52:37 +0700

    Update notify-messenger.js

--- notify-messenger.js
+++ notify-messenger.js
@@ -1,21 +1,10 @@
+onSnapshot(collection(db, "notifications"), (snap) => {
+  let count = 0;
 
-function updateNotify(count){
+  snap.forEach(doc => {
+    if(!doc.data().read) count++;
+  });
+
   localStorage.setItem("notifyCount", count);
   renderNotify();
-}
-
-function renderNotify(){
-  const count = localStorage.getItem("notifyCount") || 0;
-
-  const el = document.getElementById("notifyCount");
-  if(!el) return;
-
-  if(count > 0){
-    el.style.display = "flex";
-    el.innerText = count;
-  }else{
-    el.style.display = "none";
-  }
-}
-
-renderNotify();
+});
