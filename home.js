9877f0766f1c870af51107adca5cbb1cb1c4af4d
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-01 22:53:35 +0700

    Update home.js

--- home.js
+++ home.js
@@ -165,63 +165,76 @@ const featured = allProducts.filter(
 
     box.innerHTML += `
 
-<div class="item">
-
-    <div class="product-image">
+      <div class="item">
 
         <img
-            src="${imgUrl}"
-            onclick="goDetail('${id}')"
-            onerror="this.src='https://via.placeholder.com/300'"
-            style="cursor:pointer;"
+          src="${imgUrl}"
+
+          onclick="goDetail('${id}')"
+
+          onerror="
+            this.src=
+            'https://via.placeholder.com/300'
+          "
+
+          style="cursor:pointer;"
         >
-
-    </div>
-
-    <div class="product-info">
 
         <h4>${p.name}</h4>
 
         <div class="price-box">
 
-            <span class="price">
-                ${Number(p.price).toLocaleString()}đ
+          <span class="price">
+            ${Number(p.price)
+              .toLocaleString()}đ
+          </span>
+
+          ${
+            p.oldPrice &&
+            p.oldPrice > p.price
+
+            ?
+
+            `
+            <span class="old-price">
+              ${Number(p.oldPrice)
+                .toLocaleString()}đ
             </span>
+            `
 
-            ${
-                p.oldPrice && p.oldPrice > p.price
-                ? `
-                <span class="old-price">
-                    ${Number(p.oldPrice).toLocaleString()}đ
-                </span>
-                `
-                : ""
-            }
+            :
 
-            ${
-                percentText
-                ? `
-                <span class="discount-text">
-                    ${percentText}
-                </span>
-                `
-                : ""
-            }
+            ""
+          }
+
+          ${
+            percentText
+            ?
+            `
+            <span class="discount-text">
+              ${percentText}
+            </span>
+            `
+            :
+            ""
+          }
 
         </div>
 
         <button
-            class="cart-btn"
-            onclick="addToCart('${id}')"
+          class="cart-btn"
+          onclick="addToCart('${id}')"
         >
-            🛒 Thêm vào giỏ
+
+          🛒 Thêm vào giỏ
+
         </button>
 
-    </div>
+      </div>
 
-</div>
+    `;
 
-`;
+  });
 
   /* UPDATE */
 
