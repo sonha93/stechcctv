1705d13795effe1f10d0b87b823a97b9da3f31e0
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-02 15:41:42 +0700

    Create review-videos.js

---
+++ review-videos.js
@@ -0,0 +1,32 @@
+import { db } from "./firebase-init.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const wrap = document.getElementById("reviewVideos");
+
+const snap = await getDocs(collection(db,"products"));
+
+snap.forEach(doc=>{
+
+    const data = doc.data();
+
+    if(!data.videos?.length) return;
+
+    data.videos.forEach(video=>{
+
+        wrap.innerHTML += `
+            <div class="review-video-card">
+                <video
+                    controls
+                    preload="metadata">
+                    <source src="${video}" type="video/mp4">
+                </video>
+            </div>
+        `;
+
+    });
+
+});
