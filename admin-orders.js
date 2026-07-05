76deabbc84364e1c5459393ef6067196659f55e3
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-05 23:47:18 +0700

    Update admin-orders.js

--- admin-orders.js
+++ admin-orders.js
@@ -197,8 +197,7 @@ if (order.memberId) {
     order.userId || order.uid,
     orderId,
     title,
-    message,
-    order.items?.[0]?.img || ""
+    message
 );
   }
 
@@ -312,23 +311,25 @@ async function createNotification(userId, orderId, title, message) {
 
   if (!userId) return;
 
- async function createNotification(userId, orderId, title, message, image = "") {
-
-  if (!userId) return;
-
   await db.collection("notifications").add({
-    userId,
-    orderId,
-    title,
-    message,
-    image,
+
+    userId: userId,
+
+    orderId: orderId,
+
+    title: title,
+
+    message: message,
+
     type: "order",
+
     read: false,
+
     createdAt: firebase.firestore.FieldValue.serverTimestamp()
+
   });
 
 }
-
 // ============================
 // CHECK ADMIN
 // ============================
@@ -1613,12 +1614,11 @@ await db
 
   if (title) {
     await createNotification(
-    orderData.userId || orderData.uid,
-    id,
-    title,
-    message,
-    orderData.items?.[0]?.img || ""
-);
+      orderData.userId || orderData.uid,
+      id,
+      title,
+      message
+    );
   }
 }
         // completed => khóa
