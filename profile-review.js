3df9e725e01ded8afa01564b9432d7dd7963ad82
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-08 11:04:12 +0700

    Update profile-review.js

--- profile-review.js
+++ profile-review.js
@@ -73,7 +73,12 @@ document.getElementById("profileNameText").innerHTML =
 
 const verified = document.getElementById("verifiedBadge");
 
-verified.style.display = "block";
+if(u.verified === true){
+    verified.style.display = "block";
+}else{
+    verified.style.display = "none";
+}
+
     username.innerHTML = "@" + (u.username || "");
 
     bio.innerHTML = u.bio || "";
