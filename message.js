b6e3e06961997c5273f73e37f802673a1b72e235
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-09 23:16:54 +0700

    Update message.js

--- message.js
+++ message.js
@@ -64,8 +64,39 @@ document.getElementById("backBtn");
 // ================================
 
 let currentUser = null;
+let userCache = {};
+async function getUserInfo(uid){
 
+    if(userCache[uid]){
+        return userCache[uid];
+    }
 
+    const snap = await db
+        .collection("users")
+        .doc(uid)
+        .get();
+
+    let user = {
+        avatar: "https://i.ibb.co/Z1kv9nJj/logo.png",
+        name: "Người dùng"
+    };
+
+    if(snap.exists){
+
+        const data = snap.data();
+
+        user = {
+            avatar: data.avatar || "https://i.ibb.co/Z1kv9nJj/logo.png",
+            name: data.name || "Người dùng"
+        };
+
+    }
+
+    userCache[uid] = user;
+
+    return user;
+
+}
 
 // ================================
 // LOAD CHAT INFO
@@ -176,19 +207,21 @@ snap=>{
 
 
 
-    snap.forEach(doc=>{
+   (async()=>{
 
+    messageBox.innerHTML="";
 
-        const msg =
-        doc.data();
+    for(const doc of snap.docs){
 
+        const msg = doc.data();
 
+        await renderMessage(msg);
 
-        renderMessage(msg);
+    }
 
+    scrollBottom();
 
-
-    });
+})();
 
 
 
@@ -274,51 +307,55 @@ messageBox.appendChild(div);
 // SEND MESSAGE
 // ================================
 
-async function sendMessage(){
+async function renderMessage(msg){
 
+    const div = document.createElement("div");
 
-const text =
-messageInput.value.trim();
+    const mine = msg.senderId === currentUser.uid;
 
+    div.className = mine ? "message mine" : "message other";
 
+    let avatarHTML = "";
 
-if(!text)
-return;
+    if(!mine){
 
+        const user = await getUserInfo(msg.senderId);
 
+        avatarHTML = `
+            <img
+                class="message-avatar"
+                src="${user.avatar}"
+                onclick="location.href='profile-review.html?uid=${msg.senderId}'"
+            >
+        `;
 
-if(!currentUser)
-return;
+    }
 
+    div.innerHTML = `
 
+        ${avatarHTML}
 
-try{
+        <div class="message-body">
 
+            <div class="message-content">
 
-    const now =
-    firebase.firestore
-    .Timestamp
-    .now();
+                ${escapeHTML(msg.text || "")}
 
+            </div>
 
+            <div class="message-time">
 
-    await db
-    .collection("conversations")
-.doc(conversationId)
-.collection("messages")
-    .add({
+                ${formatTime(msg.createdAt)}
 
-        senderId:
-        currentUser.uid,
+            </div>
 
-        text:text,
+        </div>
 
-        createdAt:
-        now
+    `;
 
-    });
+    messageBox.appendChild(div);
 
-
+}
 
    await db
 .collection("conversations")
