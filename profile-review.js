e004967778bc14c42f967293b3648492498d83dd
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-10 20:54:36 +0700

    Update profile-review.js

--- profile-review.js
+++ profile-review.js
@@ -35,6 +35,9 @@ const profileUid = params.get("uid");
 // ===== HTML =====
 
 const avatar = document.getElementById("profileAvatar");
+const profileMenu = document.getElementById("profileMenu");
+const viewStoryBtn = document.getElementById("viewStoryBtn");
+const viewAvatarBtn = document.getElementById("viewAvatarBtn");
 const avatarPopup = document.createElement("div");
 
 avatarPopup.id = "avatarPopup";
@@ -777,10 +780,45 @@ if (editBtn) {
 avatar.style.cursor = "pointer";
 
 avatar.onclick = () => {
+
+    profileMenu.classList.add("active");
+
+};
+viewAvatarBtn.onclick = ()=>{
+
+    profileMenu.classList.remove("active");
+
     document.getElementById("popupAvatarImg").src = avatar.src;
+
     avatarPopup.classList.add("active");
+
 };
 
+viewStoryBtn.onclick = ()=>{
+
+    profileMenu.classList.remove("active");
+
+    if(firstStoryId){
+
+        openStory(firstStoryId);
+
+    }else{
+
+        alert("Người dùng chưa có tin.");
+
+    }
+
+};
+
+profileMenu.onclick = e=>{
+
+    if(e.target===profileMenu){
+
+        profileMenu.classList.remove("active");
+
+    }
+
+};
 document.getElementById("closeAvatarPopup").onclick = () => {
     avatarPopup.classList.remove("active");
 };
@@ -1016,7 +1054,9 @@ async function loadStories(){
     const storyBar = document.getElementById("storyBar");
 
     if(!storyBar) return;
+storyBar.innerHTML = "";
 
+firstStoryId = null;
 
     const snap = await getDocs(
         query(
@@ -1025,10 +1065,13 @@ async function loadStories(){
         )
     );
 
-    snap.forEach(docSnap=>{
+   snap.forEach(docSnap=>{
 
-        const s = docSnap.data();
+    if(!firstStoryId){
+        firstStoryId = docSnap.id;
+    }
 
+    const s = docSnap.data();
         
 
 storyBar.insertAdjacentHTML(
