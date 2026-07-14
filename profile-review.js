efb10a8236460d8dcae3284f99a767645f489634
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-14 17:06:08 +0700

    Update profile-review.js

--- profile-review.js
+++ profile-review.js
@@ -1112,14 +1112,6 @@ async function loadStories(){
     const storyBar = document.getElementById("storyBar");
 
     if(!storyBar) return;
-const mySnap = auth.currentUser
-    ? await getDoc(doc(db, "users", auth.currentUser.uid))
-    : null;
-
-const myAvatar = mySnap?.exists()
-    ? (mySnap.data().avatar || "https://i.ibb.co/Z1kv9nJj/logo.png")
-    : "https://i.ibb.co/Z1kv9nJj/logo.png";
-
 storyBar.innerHTML = `
 <div class="storyItem" id="addStoryBtn">
 
@@ -1127,14 +1119,14 @@ storyBar.innerHTML = `
 
         <img
         id="myStoryAvatar"
-        src="${myAvatar}">
+        src="${auth.currentUser?.photoURL || 'https://i.ibb.co/Z1kv9nJj/logo.png'}">
 
         <span class="storyPlus">+</span>
 
     </div>
 
     <div class="storyName">
-        
+        Story
     </div>
 
 </div>
