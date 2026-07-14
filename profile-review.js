8a08b652dc977c3c9af17fd86e514d7299a2cf2d
Hoàng Anh Sơn <sonha93dh@gmail.com>
2026-07-14 16:51:22 +0700

    Update profile-review.js

--- profile-review.js
+++ profile-review.js
@@ -1119,8 +1119,7 @@ storyBar.innerHTML = `
 
         <img
         id="myStoryAvatar"
-        src="${auth.currentUser?.photoURL || 'https://i.ibb.co/Z1kv9nJj/logo.png'}">
-
+        src="${avatar.src || 'https://i.ibb.co/Z1kv9nJj/logo.png'}"
         <span class="storyPlus">+</span>
 
     </div>
